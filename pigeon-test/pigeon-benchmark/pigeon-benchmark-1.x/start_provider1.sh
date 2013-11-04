a0566dfbe9d755e9b5c2f2bef4e592e965bff4fd
xiangwu <xiang.wu@dianping.com>
2013-11-04 17:34:48 +0800

    add benchmark scripts

--- pigeon-test/pigeon-benchmark/pigeon-benchmark-1.x/start_provider1.sh
+++ pigeon-test/pigeon-benchmark/pigeon-benchmark-1.x/start_provider1.sh
@@ -7,4 +7,4 @@ do
 done
 export CLASSPATH=$CLASSPATH
 
-nohup java -server -Xms128m -Xmx256m  -classpath $CLASSPATH  com.dianping.pigeon.test.server.SingleServer1  > ./provider1.log &
+nohup java -server -Xms128m -Xmx256m  -classpath $CLASSPATH  com.dianping.pigeon.test.server_1.x.SingleServer1  > ./provider1.log &
