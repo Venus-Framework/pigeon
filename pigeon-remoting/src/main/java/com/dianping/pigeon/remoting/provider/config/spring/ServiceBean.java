dfa1faa9a57d254ce851c43f46583c75a1ebce12
xiangwu <xiang.wu@dianping.com>
2013-11-25 10:07:38 +0800

    replace thread pool with disruptor for processing request in server-side and response in client-side

--- pigeon-remoting/src/main/java/com/dianping/pigeon/remoting/provider/config/spring/ServiceBean.java
+++ pigeon-remoting/src/main/java/com/dianping/pigeon/remoting/provider/config/spring/ServiceBean.java
@@ -4,18 +4,90 @@
  */
 package com.dianping.pigeon.remoting.provider.config.spring;
 
-import org.springframework.context.ApplicationContext;
+import org.apache.commons.lang.StringUtils;
+import org.apache.commons.lang.builder.ReflectionToStringBuilder;
+import org.apache.commons.lang.builder.ToStringStyle;
+import org.apache.log4j.Logger;
 
-/**
- * 
- * 
- * @author jianhuihuang
- * @version $Id: ServiceBean.java, v 0.1 2013-6-24 下午10:14:42 jianhuihuang Exp $
- */
-public class ServiceBean {
+import com.dianping.dpsf.exception.ServiceException;
+import com.dianping.pigeon.monitor.LoggerLoader;
+import com.dianping.pigeon.remoting.ServiceFactory;
+import com.dianping.pigeon.remoting.provider.ServerFactory;
+import com.dianping.pigeon.remoting.provider.component.ProviderConfig;
 
-	public static ApplicationContext getSpringContext() {
-		return null;
+public class NewServiceRegistry {
+
+	private static final Logger logger = LoggerLoader.getLogger(NewServiceRegistry.class);
+
+	private String url;
+	private Object serviceImpl;
+	private int port = ServerFactory.DEFAULT_PORT;
+	private String version;
+	private String interfaceName;
+
+	public String getInterfaceName() {
+		return interfaceName;
 	}
 
+	public void setInterfaceName(String interfaceName) {
+		this.interfaceName = interfaceName;
+	}
+
+	public String getVersion() {
+		return version;
+	}
+
+	public void setVersion(String version) {
+		this.version = version;
+	}
+
+	public int getPort() {
+		return port;
+	}
+
+	public void setPort(int port) {
+		this.port = port;
+	}
+
+	public String getUrl() {
+		return url;
+	}
+
+	public void setUrl(String url) {
+		this.url = url;
+	}
+
+	public Object getServiceImpl() {
+		return serviceImpl;
+	}
+
+	public void setServiceImpl(Object serviceImpl) {
+		this.serviceImpl = serviceImpl;
+	}
+
+	/**
+	 * 要确保只是启动一次！，调用Pigeon启动器，通过事件的机制来并行初始化，确保快速的启动。
+	 * 
+	 * @throws ServiceException
+	 */
+	public void init() throws Exception {
+		if (serviceImpl == null) {
+			throw new IllegalArgumentException("service not found:" + this);
+		}
+		ProviderConfig providerConfig = null;
+		if (StringUtils.isBlank(interfaceName)) {
+			providerConfig = new ProviderConfig(serviceImpl);
+		} else {
+			providerConfig = new ProviderConfig(Class.forName(interfaceName), serviceImpl);
+		}
+		providerConfig.setPort(port);
+		providerConfig.setVersion(version);
+		providerConfig.setUrl(url);
+		ServiceFactory.publishService(providerConfig);
+	}
+
+	@Override
+	public String toString() {
+		return ReflectionToStringBuilder.toString(this, ToStringStyle.SHORT_PREFIX_STYLE);
+	}
 }
