6eb4462668d30913120e71ddd78a55cd2d2adf15
xiang.wu <xiang.wu@dianping.com>
2014-04-16 15:27:41 +0800

    update version to 2.1.10

--- pigeon-common/src/main/java/com/dianping/pigeon/util/VersionUtils.java
+++ pigeon-common/src/main/java/com/dianping/pigeon/util/VersionUtils.java
@@ -2,7 +2,7 @@ package com.dianping.pigeon.util;
 
 public class VersionUtils {
 
-	public static final String VERSION = "2.1.10-SNAPSHOT";
+	public static final String VERSION = "2.1.10";
 	
 	public static int compareVersion(String version1, String version2) {
 		String[] s1 = version1.split("\\.|-");
