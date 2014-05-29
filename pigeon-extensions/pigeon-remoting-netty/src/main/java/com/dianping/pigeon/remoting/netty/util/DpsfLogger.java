c79a24ac9726129afad90a3c290cba4851eafb03
Enlight <fv3386@gmail.com>
2014-05-29 16:53:10 +0800

    add DpsfLogger

---
+++ pigeon-extensions/pigeon-remoting-netty/src/main/java/com/dianping/pigeon/remoting/netty/util/DpsfLogger.java
@@ -0,0 +1,66 @@
+package com.dianping.pigeon.remoting.netty.util;
+
+import org.apache.log4j.Logger;
+import org.jboss.netty.logging.AbstractInternalLogger;
+
+public class DpsfLogger extends AbstractInternalLogger {
+
+    private final Logger logger;
+
+    public DpsfLogger(Logger logger) {
+        this.logger = logger;
+    }
+
+    public void debug(String msg) {
+        logger.debug(msg);
+    }
+
+    public void debug(String msg, Throwable cause) {
+        logger.debug(msg, cause);
+    }
+
+    public void error(String msg) {
+        logger.error(msg);
+    }
+
+    public void error(String msg, Throwable cause) {
+        logger.error(msg, cause);
+    }
+
+    public void info(String msg) {
+        logger.info(msg);
+    }
+
+    public void info(String msg, Throwable cause) {
+        logger.info(msg, cause);
+    }
+
+    public boolean isDebugEnabled() {
+        return logger.isDebugEnabled();
+    }
+
+    public boolean isErrorEnabled() {
+        return true;
+    }
+
+    public boolean isInfoEnabled() {
+        return logger.isInfoEnabled();
+    }
+
+    public boolean isWarnEnabled() {
+        return true;
+    }
+
+    public void warn(String msg) {
+        logger.warn(msg);
+    }
+
+    public void warn(String msg, Throwable cause) {
+        logger.warn(msg, cause);
+    }
+
+    @Override
+    public String toString() {
+        return String.valueOf(logger.getName());
+    }
+}
--- pigeon-extensions/pigeon-remoting-netty/src/main/java/com/dianping/pigeon/remoting/netty/util/DpsfLoggerFactory.java
+++ pigeon-extensions/pigeon-remoting-netty/src/main/java/com/dianping/pigeon/remoting/netty/util/DpsfLoggerFactory.java
@@ -1,6 +1,5 @@
 package com.dianping.pigeon.remoting.netty.util;
 
-import org.jboss.netty.logging.Log4JLogger;
 import org.jboss.netty.logging.InternalLogger;
 import org.jboss.netty.logging.InternalLoggerFactory;
 
@@ -11,7 +10,7 @@ public class DpsfLoggerFactory extends InternalLoggerFactory {
     @Override
     public InternalLogger newInstance(String name) {
         org.apache.log4j.Logger logger = DPSFLog.getLogger();
-        return new Log4JLogger(logger);
+        return new DpsfLogger(logger);
     }
 
 }
