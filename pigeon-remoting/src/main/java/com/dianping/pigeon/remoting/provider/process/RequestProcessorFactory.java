f13a4d416463b9e2afa8cfad0bcfab6191112e90
xiang.wu <xiang.wu@dianping.com>
2015-03-30 10:53:38 +0800

    modify lazy init strategy

--- pigeon-remoting/src/main/java/com/dianping/pigeon/remoting/provider/process/RequestProcessorFactory.java
+++ pigeon-remoting/src/main/java/com/dianping/pigeon/remoting/provider/process/RequestProcessorFactory.java
@@ -4,20 +4,12 @@
  */
 package com.dianping.pigeon.remoting.provider.process;
 
-import com.dianping.pigeon.config.ConfigManagerLoader;
 import com.dianping.pigeon.remoting.provider.config.ServerConfig;
-import com.dianping.pigeon.remoting.provider.process.akka.RequestAkkaProcessor;
 import com.dianping.pigeon.remoting.provider.process.threadpool.RequestThreadPoolProcessor;
 
 public class RequestProcessorFactory {
 
-	private static final String processType = ConfigManagerLoader.getConfigManager().getStringValue(
-			"pigeon.provider.processtype", "threadpool");
-
 	public static RequestProcessor selectProcessor(ServerConfig serverConfig) {
-		if ("akka".equalsIgnoreCase(processType)) {
-			return new RequestAkkaProcessor(serverConfig);
-		}
 		return new RequestThreadPoolProcessor(serverConfig);
 	}
 }
