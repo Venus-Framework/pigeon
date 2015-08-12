7c9a0a3a9366c2cd79189971f01b31cfd8059e38
xiangwu <xiang.wu@dianping.com>
2015-08-12 16:09:53 +0800

    rm unused files

--- pigeon-common/src/main/java/com/dianping/pigeon/config/ConfigConstants.java
+++
@@ -1,8 +0,0 @@
-package com.dianping.pigeon.config;
-
-public class ConfigConstants {
-
-	public static final String ENV_PRODUCT = "product";
-	public static final String ENV_DEV = "dev";
-	public static final String ENV_ALPHA = "alpha";
-}
