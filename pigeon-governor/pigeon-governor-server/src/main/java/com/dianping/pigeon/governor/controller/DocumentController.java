b4921aa5842917d61142cc2eab90fe84fd966dbc
chongze.chen <chongze.chen@dianping.com>
2015-08-26 18:01:06 +0800

    init help page

---
+++ pigeon-governor/pigeon-governor-server/src/main/java/com/dianping/pigeon/governor/controller/DocumentController.java
@@ -0,0 +1,26 @@
+package com.dianping.pigeon.governor.controller;
+
+import java.util.HashMap;
+import java.util.Map;
+
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+
+import org.apache.logging.log4j.LogManager;
+import org.apache.logging.log4j.Logger;
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.servlet.ModelAndView;
+
+@Controller
+public class DocumentController {
+
+	private Logger log = LogManager.getLogger(this.getClass());
+	
+	@RequestMapping(value = { "/help" })
+	public ModelAndView viewHelp(HttpServletRequest request, HttpServletResponse response) {
+		Map<String, Object> map = new HashMap<String, Object>();
+		map.put("path", "help");
+		return new ModelAndView("common/main-container", map);
+	}
+}
