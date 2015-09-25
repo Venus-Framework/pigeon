1534fe1f491fd2d213e578b206ee657ffde1afd5
chongze.chen <chongze.chen@dianping.com>
2015-09-26 00:44:41 +0800

    add non auth filter

---
+++ pigeon-governor/pigeon-governor-server/src/main/java/com/dianping/pigeon/governor/filter/NonAuthFilter.java
@@ -0,0 +1,61 @@
+package com.dianping.pigeon.governor.filter;
+
+import java.io.IOException;
+
+import javax.servlet.Filter;
+import javax.servlet.FilterChain;
+import javax.servlet.FilterConfig;
+import javax.servlet.ServletException;
+import javax.servlet.ServletRequest;
+import javax.servlet.ServletResponse;
+import javax.servlet.http.HttpServletRequest;
+
+import jodd.util.StringUtil;
+
+public class NonAuthFilter implements Filter {
+	
+	private static String[] excludes;
+
+	@Override
+	public void init(FilterConfig filterConfig) throws ServletException {
+		
+		if (filterConfig != null) {
+			String ex = filterConfig.getInitParameter("excludes");
+			excludes = ex.split(",");
+		}
+		
+	}
+
+	@Override
+	public void doFilter(ServletRequest request, ServletResponse response,
+			FilterChain chain) throws IOException, ServletException {
+		HttpServletRequest req = (HttpServletRequest) request;
+		String requestURI = req.getRequestURI();
+		
+		for (String uri : excludes) {
+			
+			if (requestURI.toLowerCase().startsWith(uri)){
+				String queryString = req.getQueryString();
+				
+				if (StringUtil.isNotBlank(queryString)) {
+					requestURI = requestURI + "?" + queryString;
+				}
+				
+				request.getRequestDispatcher(requestURI).forward(request, response);
+				
+				return;
+			}
+			
+		}
+		
+		chain.doFilter(request, response);
+		
+	}
+
+	@Override
+	public void destroy() {
+		// TODO Auto-generated method stub
+		
+	}
+
+}
