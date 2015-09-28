2b9c504102c786bf57b7eb21d03e1e2b7218e518
chongze.chen <chongze.chen@dianping.com>
2015-09-28 11:10:11 +0800

    add already filtered attribute

--- pigeon-governor/pigeon-governor-server/src/main/java/com/dianping/pigeon/governor/filter/NonAuthFilter.java
+++ pigeon-governor/pigeon-governor-server/src/main/java/com/dianping/pigeon/governor/filter/NonAuthFilter.java
@@ -10,11 +10,11 @@ import javax.servlet.ServletRequest;
 import javax.servlet.ServletResponse;
 import javax.servlet.http.HttpServletRequest;
 
-import jodd.util.StringUtil;
-
 public class NonAuthFilter implements Filter {
 	
 	private static String[] excludes;
+	
+	static final String ALREADY_FILTERED_ATTRIBUTE = NonAuthFilter.class.getName() + ".FILTERED";
 
 	@Override
 	public void init(FilterConfig filterConfig) throws ServletException {
@@ -29,27 +29,34 @@ public class NonAuthFilter implements Filter {
 	@Override
 	public void doFilter(ServletRequest request, ServletResponse response,
 			FilterChain chain) throws IOException, ServletException {
-		HttpServletRequest req = (HttpServletRequest) request;
-		String requestURI = req.getRequestURI();
 		
-		for (String uri : excludes) {
-			
-			if (requestURI.toLowerCase().startsWith(uri)){
-				String queryString = req.getQueryString();
-				
-				//这里千万不要画蛇添足，会导致queryString添加两次
-				/*if (StringUtil.isNotBlank(queryString)) {
-					requestURI = requestURI + "?" + queryString;
-				}*/
-				
-				request.getRequestDispatcher(requestURI).forward(request, response);
-				
-				return;
-			}
-			
-		}
-		
-		chain.doFilter(request, response);
+		if (request.getAttribute(ALREADY_FILTERED_ATTRIBUTE) != null) {
+            chain.doFilter(request, response);
+            
+        } else {
+            request.setAttribute(ALREADY_FILTERED_ATTRIBUTE, Boolean.TRUE);
+            
+    		String requestURI = ((HttpServletRequest) request).getRequestURI();
+    		
+    		for (String uri : excludes) {
+    			
+    			if (requestURI.toLowerCase().startsWith(uri)){
+    				
+    				//这里千万不要画蛇添足，会导致queryString添加两次
+    				/*String queryString = req.getQueryString();
+    				if (StringUtil.isNotBlank(queryString)) {
+    					requestURI = requestURI + "?" + queryString;
+    				}*/
+    				
+    				request.getRequestDispatcher(requestURI).forward(request, response);
+    				
+    				return;
+    			}
+    			
+    		}
+    		
+    		chain.doFilter(request, response);
+        }
 		
 	}
 
