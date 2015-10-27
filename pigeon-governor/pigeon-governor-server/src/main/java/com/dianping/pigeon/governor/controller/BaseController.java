5a41a92123c57971eb5245829d918ccd60f3e13c
chongze.chen <chongze.chen@dianping.com>
2015-10-27 11:39:33 +0800

    fix

--- pigeon-governor/pigeon-governor-server/src/main/java/com/dianping/pigeon/governor/controller/BaseController.java
+++ pigeon-governor/pigeon-governor-server/src/main/java/com/dianping/pigeon/governor/controller/BaseController.java
@@ -26,22 +26,22 @@ public class BaseController {
 		modelMap.addAttribute("currentUser", currentUser);
 	}
 
-	public void verifyIdentity(int userId) throws Exception{
+	public void verifyIdentity(int agentId) throws Exception{
 
-		if(userId != 3){
-			throw new Exception(String.format("User id %d is not system level", userId));
+		if(3 != agentId){
+			throw new Exception(String.format("agentId %d is not system level", agentId));
 		}
 
 	}
 
-	public void verifyIdentity(HttpServletRequest request, int userId) throws Exception{
+	public void verifyIdentity(HttpServletRequest request, int agentId) throws Exception{
 		
 		String ip = IPUtils.getUserIP(request);
 		if(!"0:0:0:0:0:0:0:1%0".equals(ip)){
 			throw new Exception("You have no privilege.");
 		}
 		
-		verifyIdentity(userId);
+		verifyIdentity(agentId);
 		
 	}
 }
