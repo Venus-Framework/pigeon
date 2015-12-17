a9c6b3b69acbe80553d0885200ba96533add30f5
chongze.chen <chongze.chen@dianping.com>
2015-12-17 11:51:13 +0800

    add abstract class

--- pigeon-remoting/src/main/java/com/dianping/pigeon/remoting/invoker/proxy/ServiceProxy.java
+++ pigeon-remoting/src/main/java/com/dianping/pigeon/remoting/invoker/proxy/ServiceProxy.java
@@ -3,6 +3,7 @@ package com.dianping.pigeon.remoting.invoker.proxy;
 import com.dianping.pigeon.remoting.invoker.config.InvokerConfig;
 
 import java.util.Map;
+import java.util.concurrent.ConcurrentHashMap;
 
 /**
  * Created by chenchongze on 15/12/16.
