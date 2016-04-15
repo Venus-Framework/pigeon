465a5c33aff200f5222ba58e5224028c062092ed
chongze.chen <chongze.chen@dianping.com>
2016-04-15 18:12:08 +0800

    fix

--- pigeon-remoting/src/main/java/com/dianping/pigeon/remoting/invoker/proxy/AbstractServiceProxy.java
+++ pigeon-remoting/src/main/java/com/dianping/pigeon/remoting/invoker/proxy/AbstractServiceProxy.java
@@ -1,6 +1,7 @@
 package com.dianping.pigeon.remoting.invoker.proxy;
 
 import com.dianping.pigeon.log.LoggerLoader;
+import com.dianping.pigeon.remoting.invoker.exception.RegionException;
 import com.dianping.pigeon.remoting.invoker.region.RegionManager;
 import com.dianping.pigeon.remoting.ServiceFactory;
 import com.dianping.pigeon.remoting.common.codec.SerializerFactory;
@@ -10,6 +11,7 @@ import com.dianping.pigeon.remoting.invoker.ClientManager;
 import com.dianping.pigeon.remoting.invoker.InvokerBootStrap;
 import com.dianping.pigeon.remoting.invoker.config.InvokerConfig;
 import com.dianping.pigeon.remoting.invoker.route.balance.LoadBalanceManager;
+import com.dianping.pigeon.remoting.invoker.route.region.RegionPolicyManager;
 import org.apache.commons.lang.StringUtils;
 import org.apache.logging.log4j.Logger;
 
@@ -24,7 +26,7 @@ public abstract class AbstractServiceProxy implements ServiceProxy {
     protected static Map<InvokerConfig<?>, Object> services = new ConcurrentHashMap<InvokerConfig<?>, Object>();
     protected Logger logger = LoggerLoader.getLogger(this.getClass());
 
-    private RegionManager regionManager = RegionManager.INSTANCE;
+    private final RegionPolicyManager regionPolicyManager = RegionPolicyManager.INSTANCE;
 
     @Override
     public void init() {
@@ -60,13 +62,12 @@ public abstract class AbstractServiceProxy implements ServiceProxy {
                 throw new RpcException("error while trying to get service:" + invokerConfig, t);
             }
 
-            //TODO 考虑这里开始添加动态监控region的任务
+            // setup region policy for service
             try {
-                if(regionManager.isEnableRegionAutoSwitch()) {
-                    regionManager.register(invokerConfig.getUrl());
-                }
+                regionPolicyManager.register(invokerConfig.getUrl(), invokerConfig.getGroup(),
+                        invokerConfig.getRegionPolicy());
             } catch (Throwable t) {
-                logger.warn("error while setup region manager: " + invokerConfig, t);
+                throw new RegionException("error while setup region route policy: " + invokerConfig, t);
             }
 
             try {
