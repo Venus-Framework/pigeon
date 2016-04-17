ea78d3be075f2ad583cfcf7b8a2b0f3cf4261c4e
chongze.chen <chongze.chen@dianping.com>
2016-04-17 12:03:59 +0800

    autoswitch region policy impl -- fix

--- pigeon-remoting/src/main/java/com/dianping/pigeon/remoting/invoker/route/region/AutoSwitchRegionPolicy.java
+++ pigeon-remoting/src/main/java/com/dianping/pigeon/remoting/invoker/route/region/AutoSwitchRegionPolicy.java
@@ -5,21 +5,12 @@ import com.dianping.pigeon.config.ConfigManagerLoader;
 import com.dianping.pigeon.domain.HostInfo;
 import com.dianping.pigeon.log.LoggerLoader;
 import com.dianping.pigeon.registry.RegistryManager;
-import com.dianping.pigeon.remoting.ServiceFactory;
 import com.dianping.pigeon.remoting.invoker.Client;
 import com.dianping.pigeon.remoting.invoker.ClientManager;
 import com.dianping.pigeon.remoting.invoker.config.InvokerConfig;
-import com.dianping.pigeon.remoting.invoker.exception.RegionException;
-import com.dianping.pigeon.threadpool.DefaultThreadFactory;
-import com.dianping.pigeon.util.ServiceUtils;
-import org.apache.commons.lang.StringUtils;
 import org.apache.logging.log4j.Logger;
 
 import java.util.*;
-import java.util.concurrent.ConcurrentHashMap;
-import java.util.concurrent.ConcurrentMap;
-import java.util.concurrent.ExecutorService;
-import java.util.concurrent.Executors;
 
 /**
  * Created by chenchongze on 16/4/15.
@@ -46,115 +37,12 @@ public class AutoSwitchRegionPolicy implements RegionPolicy {
     //TODO 加入动态变化
     private float regionSwitchRatio = configManager.getFloatValue("pigeon.regions.switchratio", 0.5f);
 
-    // serviceId --> regionHeartBeatList
-    private ConcurrentMap<String, List<InnerRegionHeartBeatStat>> regionHeartBeatStats
-            = new ConcurrentHashMap<String, List<InnerRegionHeartBeatStat>>();
-
-    private AutoSwitchRegionPolicy() {
-        try {
-            ExecutorService regionAutoSwitchThreadPool = Executors.newFixedThreadPool(1,
-                    new DefaultThreadFactory("Pigeon-Client-RegionAutoSwitch-ThreadPool"));
-            regionAutoSwitchThreadPool.submit(new InnerCheckTask());
-        } catch (Throwable t) {
-            throw new RegionException("Init autoSwitchRegionTask failed!", t);
-        }
-    }
+    private AutoSwitchRegionPolicy() {}
 
     @Override
     public List<Client> getPreferRegionClients(List<Client> clientList, InvokerConfig<?> invokerConfig) {
         //TODO 自动开关关闭时，返回用户指定region的所有clients
         return getRegionActiveClients(clientList);
-    }
-
-    private class InnerCheckTask implements Runnable {
-
-        @Override
-        public void run() {
-            long sleepTime = 1000L;
-            while (!Thread.currentThread().isInterrupted()) {
-                try {
-                    Thread.sleep(sleepTime);
-                    //检查region路由开关
-                    if(!regionPolicyManager.isEnableRegionPolicy()) { continue; }
-
-                    //取出采用当前策略的services
-                    Set<InvokerConfig<?>> services = ServiceFactory.getAllServiceInvokers().keySet();
-                    Map<String, String> serviceGroupMap = new HashMap<String, String>();
-                    for (InvokerConfig<?> invokerConfig : services) {
-                        //TODO 待验证
-                        if(!regionPolicyManager.getRegionPolicy(invokerConfig).equals(this)) {
-                            continue;
-                        }
-                        String vip = "";
-                        if (StringUtils.isNotBlank(invokerConfig.getVip())) {
-                            vip = invokerConfig.getVip();
-                        }
-                        serviceGroupMap.put(invokerConfig.getUrl(), invokerConfig.getGroup() + "#" + vip);
-
-                        getRegionHeartBeatStatWithCreate(ServiceUtils.getServiceId(invokerConfig.getUrl(),
-                                invokerConfig.getGroup()));
-                    }
-
-                    long now = System.currentTimeMillis();
-                    for (final String url : serviceGroupMap.keySet()) {
-                        String groupValue = serviceGroupMap.get(url);
-                        String group = groupValue.substring(0, groupValue.lastIndexOf("#"));
-                        String vip = groupValue.substring(groupValue.lastIndexOf("#") + 1);
-                        if (vip != null && vip.startsWith("console:")) {
-                            continue;
-                        }
-
-                        final int priority = regionManager.getCurrentRegion(url).getPriority();
-                        final Region[] regionArray = regionPolicyManager.getRegionArray();
-
-                        // 检查当前region的前置region
-                        for(int i = 0; i < priority; ++i) {
-                            int total = getRegionTotalClients(url, regionArray[i]);
-                            int available = getRegionAvailableClients(url, regionArray[i]);
-
-                            if(total > 0 && available >= regionSwitchRatio * total) {
-                                //有恢复，切换，关闭后置连接，break
-                                regionManager.switchRegion(url, regionArray[i]);
-                                logger.info("[region-switch] auto switch region to " + regionArray[i]);
-
-                                break;
-                            }
-                        }
-
-                        if(priority == regionManager.getCurrentRegion(url).getPriority()) {// 没有切换或切换失败，判断是否切换后置region
-                            int total = getRegionTotalClients(url, regionArray[priority]);
-                            int available = getRegionAvailableClients(url, regionArray[priority]);
-                            if(total == 0 || available < regionSwitchRatio * total) {
-                                final int candidate = priority + 1;
-                                if(candidate < regionArray.length) {
-                                    regionManager.switchRegion(url, regionArray[candidate]);
-                                    logger.info("[region-switch] auto switch region to " + regionArray[candidate]);
-
-                                    String error = null;
-                                    try {
-                                        ClientManager.getInstance().registerClients(url, group, vip);
-                                    } catch (Throwable e) {
-                                        error = e.getMessage();
-                                    }
-                                    if (error != null) {
-                                        logger.warn("[provider-available] failed to get providers, caused by:" + error);
-                                    }
-                                }
-                            }
-                        }
-
-                    }
-
-                    sleepTime = interval - (System.currentTimeMillis() - now);
-                } catch (Throwable e) {
-                    logger.warn("[region_change] task failed:", e);
-                } finally {
-                    if (sleepTime < 1000) {
-                        sleepTime = 1000;
-                    }
-                }
-            }
-        }
     }
 
     private List<Client> getRegionActiveClients(List<Client> clientList) {
@@ -210,32 +98,6 @@ public class AutoSwitchRegionPolicy implements RegionPolicy {
             }
         }
         return total > 0 && active >= regionSwitchRatio * total;
-    }
-
-    private List<InnerRegionHeartBeatStat> getRegionHeartBeatStatWithCreate(String serviceId) {
-        List<InnerRegionHeartBeatStat> heartBeatStatList = regionHeartBeatStats.get(serviceId);
-        if (heartBeatStatList == null) {
-            List<InnerRegionHeartBeatStat> _heartBeatStatList = new ArrayList<InnerRegionHeartBeatStat>();
-            heartBeatStatList = regionHeartBeatStats.putIfAbsent(serviceId, _heartBeatStatList);
-            if (heartBeatStatList == null) {
-                heartBeatStatList = _heartBeatStatList;
-            }
-        }
-        return heartBeatStatList;
-    }
-
-    private class InnerRegionHeartBeatStat {
-        private final String name;
-        private final int priority;
-        private boolean active = true;
-        private float liveRate = 1f;
-
-        public InnerRegionHeartBeatStat(String name, int priority) {
-            this.name = name;
-            this.priority = priority;
-        }
-
-
     }
 
     private class InnerRegionstat {
