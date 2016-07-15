c71449fba94b4ac958e6af50db5f9e7a38148240
ChenChongze <chenchongze@CCZMBP.local>
2016-07-15 14:47:03 +0800

    update mns invoker interface

--- pigeon-extensions/pigeon-registry-mns/src/main/java/com/dianping/pigeon/registry/mns/MnsUtils.java
+++ pigeon-extensions/pigeon-registry-mns/src/main/java/com/dianping/pigeon/registry/mns/MnsUtils.java
@@ -5,15 +5,26 @@ package com.dianping.pigeon.registry.mns;
  */
 public class MnsUtils {
 
+
+    /**
+     * 注册服务, uptCmd:
+     * 0,重置(代表后面的serviceName list就是该应用支持的全量接口)，
+     * 1，增加(代表后面的serviceName list是该应用新增的接口)，
+     * 2，减少(代表后面的serviceName list是该应用删除的接口)。
+     */
+    public final static int UPT_CMD_RESET = 0;
+    public final static int UPT_CMD_ADD = 1;
+    public final static int UPT_CMD_DEL = 2;
+
     public static int getMtthriftStatus(int pigeon_weight) {
         int status = 0;
 
         if (pigeon_weight < 0) {
-            status = 4;
+            status = 4;//stopped
         } else if (pigeon_weight == 0) {
-            status = 0;
+            status = 0;//dead
         } else if (pigeon_weight > 0) {
-            status = 2;
+            status = 2;//alive
         }
 
         return status;
